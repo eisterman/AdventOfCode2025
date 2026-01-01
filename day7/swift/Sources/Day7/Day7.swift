2c9b6ba7cc3b53693320a8932980198eafca54d5
Federico Pasqua <federico.pasqua.96@gmail.com>
2026-01-01 17:31:19 +0100

    Day7 Swift: refactor problem 1 as more idiomatic swift code

--- day7/swift/Sources/Day7/Day7.swift
+++ day7/swift/Sources/Day7/Day7.swift
@@ -3,23 +3,9 @@
 
 import Foundation
 
-class Point: Hashable, Equatable, CustomStringConvertible {
-    // Class is REFERENCE-LIKE, while Struct is VALUE-LIKE
+struct Point: Hashable, CustomStringConvertible {
     var x: Int
     var y: Int
-    init(x: Int, y: Int) {
-        self.x = x
-        self.y = y
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(x)
-        hasher.combine(y)
-    }
-
-    static func == (lhs: Point, rhs: Point) -> Bool {
-        lhs.x == rhs.x && lhs.y == rhs.y
-    }
 
     var description: String {
         "(\(self.x),\(self.y))"
@@ -33,31 +19,41 @@ struct Day7 {
             // let content = try String.init(contentsOfFile: "../test.data", encoding: .utf8)
             let content = try String.init(contentsOfFile: "../input.data", encoding: .utf8)
             let grid: [[Character]] = content.split(separator: "\n").map({ Array($0) })  // grid[y][x]
-            var particles: [Point] = []
-            // FInd initial particle
-            let sx = grid[0].firstIndex(of: "S")!
-            particles.append(Point(x: sx, y: 0))
-            var res1 = 0
-            while !particles.allSatisfy({ $0.y == grid.count - 1 }) {
-                var new_parts: [Point] = []
-                for i in 0..<particles.count {
-                    let p = particles[i]
-                    if grid[p.y + 1][p.x] == "^" {
-                        new_parts.append(Point(x: p.x + 1, y: p.y + 1))
-                        p.y += 1
-                        p.x -= 1
-                        res1 += 1
-                    } else {
-                        p.y += 1
-                    }
-                }
-                particles.append(contentsOf: new_parts)
-                // Remove duplicates
-                particles = Array(Set(particles))
-            }
-            print("Problem 1: \(res1)")
+            solvePart1(grid: grid)
         } catch {
             print("error \(error)")
         }
+    }
+
+    static func solvePart1(grid: [[Character]]) {
+        // Used for optional unwrapping and early exit. It's the opposite of if let
+        // guard condition else {} ==> if !condition {}
+        guard let startX = grid[0].firstIndex(of: "S") else {
+            print("No start position found")
+            return
+        }
+        var particles: Set<Point> = [Point(x: startX, y: 0)]
+        var splitCount = 0
+        let maxY = grid.count - 1
+        while !particles.allSatisfy({ $0.y == maxY }) {
+            var newParticles: Set<Point> = []
+            for particle in particles {
+                guard particle.y < maxY else {
+                    newParticles.insert(particle)
+                    print("Particle \(particle) already at the end")
+                    continue
+                }
+                let nextY = particle.y + 1
+                if grid[nextY][particle.x] == "^" {
+                    newParticles.insert(Point(x: particle.x - 1, y: nextY))
+                    newParticles.insert(Point(x: particle.x + 1, y: nextY))
+                    splitCount += 1
+                } else {
+                    newParticles.insert(Point(x: particle.x, y: nextY))
+                }
+            }
+            particles = newParticles
+        }
+        print("Problem 1: \(splitCount)")
     }
 }
